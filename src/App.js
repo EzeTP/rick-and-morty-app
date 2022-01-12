8dbfa9ac23253e4deb8683ab3fe148d3bef6846a
EzeTP <tejada.eap@gmail.com>
2022-01-12 14:19:11 -0300

    starting a rick and morty's app using, React, Bootstrap and Sass

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-center poppins my-4"  >
+        Rick and Morty <span className="text-success fw-bold">Wiki</span>
+       </h1>
     </div>
+
   );
 }
 
